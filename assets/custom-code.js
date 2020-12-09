c9a4fc7de6793d102ce89e66369ab28afa7fae1b
Teran <teran@pop-os.localdomain>
2020-12-09 16:49:07 -0700

    Animated projects

--- assets/custom-code.js
+++ assets/custom-code.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     var bash_tags = document.querySelectorAll("code.language-bash")
-    console.log("Hi")
     bash_tags.forEach(
         function (currValue, currIndex, listObj) {
             currValue.innerHTML="<span class='bash'>"+(currValue.textContent.split("\n").filter(Boolean).join("</span>\n<span class='bash'>"))+"</span>";
